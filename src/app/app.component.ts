412f8a64b4b0f4bcc47c20dd7fded9a0dd7ad9ff
Stéphane Francel <stephane.francel@zenika.com>
2024-08-09 15:13:46 +0200

    Control flow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LazyComponent } from './lazy/lazy.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LazyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  bool = true;
+
+  fruits = [
+    { id: 1, value: 'banane' },
+    { id: 2, value: 'pomme' },
+  ];
+}
